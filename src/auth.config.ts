a1b7e97d77aa9135aadbe6db61f99a8243345824
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-30 02:20:41 +0400

    s

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -4,6 +4,23 @@ import Credentials from "next-auth/providers/credentials";
 export default {
   providers: [
     Credentials({
+      async authorize() { return null; }
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = (user as any).role;
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.role = token.role as any;
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
 } satisfies NextAuthConfig;
